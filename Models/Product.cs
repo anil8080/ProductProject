e0b48352a4b19e2ceccb1e75ad07089b290769f7
anil8080 <119519987+anil8080@users.noreply.github.com>
2022-11-30 22:37:00 +0530

    Add files via upload

---
+++ Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ProductsProject.Models
+{
+    [Table("tblProducts")]
+    public class Product
+    {  
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public string ProductName { get; set; }
+
+        public virtual Category Category { get; set;}
+        public int CategoryId { get; set; }
+    }
+}
